aaa09468e89e49680be739795f9cb5e881a3f539
gbarthel3636 <george.barthel@powerhrg.com>
2019-03-25 19:18:29 -0400

    add anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(n)
+    n.select do |n|
+      n.split("").sort == @anagram.split("").sort
+    end
+  end
+end
