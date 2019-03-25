5a15e2665fb588ee5888336d1700adc6d5581e12
KFad <kfadigan@yahoo.com>
2019-03-25 11:07:38 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arguments)
+    arguments.select do |a|
+      word.split("").sort == a.split("").sort
+      binding.pry
+    end
+  end
+
+end
